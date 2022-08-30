4dbae92bdea95061e2efbc438905fd0cedcebb4e
corn1200 <corn120033@gmail.com>
2022-08-30 16:22:11 +0900

    초기 커밋.

---
+++ Baekjoon/9012_bracket.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int totalKilogram = 0;
+	int fiveKiloCount = 0;
+	int threeKiloCount = 0;
+
+	cin >> totalKilogram;
+	fiveKiloCount = totalKilogram / 5;
+
+	if (totalKilogram % 5 == 0) {
+		cout << fiveKiloCount;
+	}
+	else {
+		int restKilo = 0;
+		for (fiveKiloCount; fiveKiloCount > -1; fiveKiloCount--)
+		{
+			restKilo = totalKilogram - (fiveKiloCount * 5);
+			//cout << restKilo << "\n";
+			threeKiloCount = restKilo / 3;
+			if (restKilo % 3 == 0) {
+				int totalCount = fiveKiloCount + threeKiloCount;
+				cout << totalCount << "\n";
+				return 0;
+			}
+		}
+		cout << "-1";
+	}
+	return 0;
+}
