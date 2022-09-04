662e7e610762f0d34a97a52a506d125337bfbeb0
김현승 <corn120033@gmail.com>
2022-09-05 02:49:43 +0900

    24416번 '알고리즘 수업 - 피보나치 수 1' 메모리 개선

--- Baekjoon/dynamic_programming/9184_excitingFunctionExecution.cpp
+++ Baekjoon/dynamic_programming/9184_excitingFunctionExecution.cpp
@@ -8,41 +8,22 @@ int w(int a, int b, int c) {
 		return 1;
 	}
 	if (a > 20 || b > 20 || c > 20) {
-		if (wMemory[20][20][20] == 0) {
-			wMemory[20][20][20] = w(20, 20, 20);
-		}
-		return wMemory[20][20][20];
+		return w(20, 20, 20);
+	}
+	if (wMemory[a][b][c] != 0) {
+		return wMemory[a][b][c];
 	}
 	if (a < b && b < c) {
-		if (wMemory[a][b][c - 1] == 0) {
-			wMemory[a][b][c - 1] = w(a, b, c - 1);
-		}
-		if (wMemory[a][b - 1][c - 1] == 0) {
-			wMemory[a][b - 1][c - 1] = w(a, b - 1, c - 1);
-		}
-		if (wMemory[a][b - 1][c] == 0) {
-			wMemory[a][b - 1][c] = w(a, b - 1, c);
-		}
-		return wMemory[a][b][c - 1]
-			+ wMemory[a][b - 1][c - 1]
-			- wMemory[a][b - 1][c];
+		return wMemory[a][b][c] =
+			w(a, b, c - 1)
+			+ w(a, b - 1, c - 1)
+			- w(a, b - 1, c);
 	}
-	if (wMemory[a - 1][b][c] == 0) {
-		wMemory[a - 1][b][c] = w(a - 1, b, c);
-	}
-	if (wMemory[a - 1][b - 1][c] == 0) {
-		wMemory[a - 1][b - 1][c] = w(a - 1, b - 1, c);
-	}
-	if (wMemory[a - 1][b][c - 1] == 0) {
-		wMemory[a - 1][b][c - 1] = w(a - 1, b, c - 1);
-	}
-	if (wMemory[a - 1][b - 1][c - 1] == 0) {
-		wMemory[a - 1][b - 1][c - 1] = w(a - 1, b - 1, c - 1);
-	}
-	return wMemory[a - 1][b][c]
-		+ wMemory[a - 1][b - 1][c]
-		+ wMemory[a - 1][b][c - 1]
-		- wMemory[a - 1][b - 1][c - 1];
+	return wMemory[a][b][c] =
+		w(a - 1, b, c)
+		+ w(a - 1, b - 1, c)
+		+ w(a - 1, b, c - 1)
+		- w(a - 1, b - 1, c - 1);
 }
 
 int main() {
